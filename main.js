8e5ee25eba4a6a49669ac61197027f56e4291cfa
Ben Patterson <bmizepatterson@gmail.com>
2018-10-19 14:51:13 -0400

    Use fetch for API request

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ let app = new Vue({
 
     data: {
         view: 'grid',
-        req: new XMLHttpRequest(),
         gifs: null,
         queryInput: '',
         apiKey: 'lr7xVquFU0B9lugQ0Paur26Ckg89Cr1N',
         error: false,
-        reqCount: 0,
         pageSize: 5,
         mode: 'gifs'
     },
@@ -24,9 +22,6 @@ let app = new Vue({
         let savedMode = localStorage.getItem("mode");
         if (savedMode) this.mode = savedMode;
 
-
-        this.req.onload = this.onSuccess;
-        this.req.onerror = this.onError;
         this.makeRequest();
     },
 
@@ -40,12 +35,19 @@ let app = new Vue({
             return this.view == 'list';
         },
 
-        baseUrl: function() {
+        fetchUrl: function() {
+            let url = '';
             if (this.query) {
-                return 'https://api.giphy.com/v1/' + this.mode + '/search?';
+                url += 'https://api.giphy.com/v1/' + this.mode + '/search?';
             } else {
-                return 'https://api.giphy.com/v1/' + this.mode + '/trending?';
+                url += 'https://api.giphy.com/v1/' + this.mode + '/trending?';
             }
+            if (this.query) {
+                url += `q=${this.query}&`;
+            }
+            url += 'api_key=' + this.apiKey;
+            url += '&limit=' + this.pageSize;
+            return url;
         },
 
         noResults: function() {
@@ -54,7 +56,6 @@ let app = new Vue({
 
         query: function() {
             if (!this.queryInput) return '';
-
             return encodeURI(this.queryInput.trim());
         }
     },
@@ -96,28 +97,19 @@ let app = new Vue({
             this.makeRequest();
         },
 
-        onError: function() {
-            this.error = true;
-        },
-
-        onSuccess: function() {
-            if (this.req.status == '200') {
-                this.gifs = JSON.parse(this.req.responseText).data;
-            } else {
-                this.onError();
-            }
-        },
-
         makeRequest: _.debounce(function() {
-            let url = this.baseUrl;
-            if (this.query) {
-                url += `q=${this.query}&`;
-            }
-            url += 'api_key=' + this.apiKey;
-            url += '&limit=' + this.pageSize;
-            this.req.open('get', url, true);
-            this.req.send();
-            this.reqCount++;
+            fetch(this.fetchUrl)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        return Promise.reject();
+                    }
+                })
+                .then(data => this.gifs = data.data)
+                .catch( () => {
+                    this.error = true;
+                });
         }, 500)
     }
 })
